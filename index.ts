28dbbc09d963b078569d329c3fad290ae505f366
MOZGIII <mike-n@narod.ru>
2024-11-27 12:29:00 -0300

    More explicit env var loading

--- index.ts
+++ index.ts
@@ -4,17 +4,30 @@ import type { Update } from "tdlib-types";
 
 tdl.configure({ tdjson: getTdjson() });
 
+const env = (key: string): string => {
+  const val = process.env[key];
+  if (val === undefined) {
+    throw new Error(`The env var ${key} is not set.`);
+  }
+  return val;
+};
+
+const TG_API_ID = env("TG_API_ID");
+const TG_API_HASH = env("TG_API_HASH");
+const TG_BOT_TOKEN = env("TG_BOT_TOKEN");
+const TG_BOT_ADMIN_USER_ID = env("TG_BOT_ADMIN_USER_ID");
+
 const client = tdl.createClient({
-  apiId: Number(process.env.TG_API_ID!),
-  apiHash: process.env.TG_API_HASH!,
+  apiId: Number(TG_API_ID),
+  apiHash: TG_API_HASH!,
 });
 
-const BOT_ADMIN_USER_ID = Number(process.env.TG_BOT_ADMIN_USER_ID!);
+const botAdminUserId = Number(TG_BOT_ADMIN_USER_ID);
 
 client.on("error", console.error);
 
-const exportMembers = async (chat_id: number) => {
-  const chat = await client.invoke({ _: "getChat", chat_id });
+const exportMembers = async (chatId: number) => {
+  const chat = await client.invoke({ _: "getChat", chat_id: chatId });
 
   if (chat.type._ !== "chatTypeSupergroup") {
     throw new Error("not a supergroup");
@@ -61,7 +74,7 @@ const handleUpdate = async (update: Update) => {
 
         if (
           update.message.sender_id._ === "messageSenderUser" &&
-          update.message.sender_id.user_id === BOT_ADMIN_USER_ID
+          update.message.sender_id.user_id === botAdminUserId
         ) {
           await exportMembers(chat_id);
         } else {
@@ -89,7 +102,7 @@ const handleUpdate = async (update: Update) => {
 };
 
 async function main() {
-  await client.loginAsBot(process.env.TG_BOT_TOKEN!);
+  await client.loginAsBot(TG_BOT_TOKEN);
 
   const me = await client.invoke({ _: "getMe" });
   console.log("My user:", me.id);
